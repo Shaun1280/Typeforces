6b0d1e7ca6086ca41076eb75f124e102e44eb0ed
Shaun-Shane <1953181@tongji.edu.cn>
2022-04-12 15:00:39 +0800

    update registercontrollerpolicy

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -2,13 +2,11 @@ const { User } = require('../models')
 
 module.exports = {
   async register (req, res) {
-    console.log(req.body)
     try {
       const user = await User.create(req.body)
       res.send(user.toJSON())
     } catch (err) {
       // email already exist
-      console.log(err);
       res.status(400).send({
         error: 'This email account is aleady in use'
       })
--- server/src/policies/AuthenticationControllerPolicy.js
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -1,7 +1,42 @@
-const Joi = require('joi');
+const Joi = require('joi')
 
-module.expors = {
-  register (req, res, next) {
-    next()
+module.exports = {
+  async register (req, res, next) {
+    const schema = Joi.object({
+      email: Joi.string().email(),
+      password: Joi.string().regex(
+        /^[a-zA-Z0-9]{8,32}/
+      )
+    })
+
+    try {
+      await schema.validateAsync(req.body);
+      next()
+    } catch (error) {
+      console.log(error)
+      switch (error.details[0].context.key) {
+        case 'email':
+          res.status(400).send({
+            error: 'You must provide a validate email address!'
+          })
+          break
+
+        case 'password':
+          res.status(400).send({
+            error: `The password provided failed to match the following rules:
+              <br>
+              1. It must contain only the following characters: a-z, A-Z, 0-9.
+              <br>
+              2. It must be at least 8 characters in length and not greater than 32 characters in length.
+            `
+          })
+          break
+
+        default:
+          res.status(400).send({
+            error: 'Invalid registration information'
+          })
+      }
+    }
   }
 }
