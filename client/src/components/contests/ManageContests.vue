c810cc8c0b43b9b9bcaeeffe6b8b0f5b6ad14a88
Shaun-Shane <1953181@tongji.edu.cn>
2022-05-02 17:16:32 +0800

    manage contest creation

--- client/src/components/contests/ManageContests.vue
+++ client/src/components/contests/ManageContests.vue
@@ -1,19 +1,184 @@
 <template>
-  <v-layout class="d-flex justify-center">
+  <v-layout class="d-flex flex-column justify-center">
+    <!-- buttons -->
+      <v-flex class="text-center">
+        <v-btn
+          class="mx-2"
+          fab
+          dark
+          color="indigo"
+          title="Add Contest"
+        >
+          <v-icon dark>
+            mdi-plus
+          </v-icon>
+        </v-btn>
+
+        <v-btn
+          class="mx-2"
+          fab
+          dark
+          color="red"
+          title="Delete Upcoming Contest"
+        >
+          <v-icon dark>
+            mdi-delete
+          </v-icon>
+        </v-btn>
+
+        <v-btn
+          class="mx-2"
+          fab
+          dark
+          large
+          color="cyan"
+          title="Modify Contest"
+        >
+          <v-icon dark>
+            mdi-pencil
+          </v-icon>
+        </v-btn>
+      </v-flex>
+
+    <v-flex row class="justify-center mt-10">
+      <v-flex xs4>
+        <pannel title="New Contest">
+          <v-text-field
+            label="RoundName"
+            v-model="contest.round_name"
+          ></v-text-field>
+          <br />
+
+          <!-- calendar -->
+          <v-menu
+            ref="menu1"
+            v-model="menu1"
+            :close-on-content-click="false"
+            :return-value.sync="date"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="date"
+                label="Picker in menu"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="date"
+              no-title
+              scrollable
+            >
+              <v-spacer></v-spacer>
+              <v-btn
+                text
+                color="primary"
+                @click="menu1 = false"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                text
+                color="primary"
+                @click="$refs.menu1.save(date)"
+              >
+                OK
+              </v-btn>
+            </v-date-picker>
+          </v-menu>
+
+          <!-- time-picker -->
+          <v-menu
+            ref="menu2"
+            v-model="menu2"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            :return-value.sync="time"
+            transition="scale-transition"
+            offset-y
+            max-width="290px"
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="time"
+                label="Picker in menu"
+                prepend-icon="mdi-clock-time-four-outline"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-time-picker
+              v-if="menu2"
+              v-model="time"
+              full-width
+              @click:minute="$refs.menu2.save(time)"
+            ></v-time-picker>
+          </v-menu>
+
+          <v-autocomplete
+            :items="['Div1', 'Div2', 'Div1 + Div2']"
+            label="Division"
+            v-model="contest.division"
+          ></v-autocomplete>
+          <br />
+        </pannel>
+      </v-flex>
+
+      <v-flex xs6>
+        <pannel title="New Contest Content" class="ml-4">
+          <v-textarea
+            label="Content"
+            v-model="contest.content"
+          ></v-textarea>
+
+          <v-btn @click="createContest">Add Contest</v-btn>
+        </pannel>
+      </v-flex>
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
+import ContestServices from '@/services/ContestServices'
+
 export default {
   data () {
+    // Date.parse(new Date("2022/05/02 00:00:00"))
     return {
-      email: '',
-      password: '',
-      error: null
+      date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+      menu1: false,
+      time: null,
+      menu2: false,
+      operation_type: 1, // 1 add, 2 delete, 3 update
+      contest: {
+        round_name: null,
+        start_time: null,
+        content: null,
+        division: null,
+        error: null
+      }
     }
   },
   methods: {
-
+    async createContest () {
+      // call api
+      try {
+        // this.date = (new Date(new Date('2022/05/02 00:00:00') - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10)
+        // this.time = `12:30`
+        console.log(this.contest.division)
+        this.contest.start_time = Date.parse(`${this.date} ${this.time}`)
+        await ContestServices.post(this.contest)
+      } catch (err) {
+        console.log(err)
+      }
+    }
   }
   // watch: {
   //   email (value) {
--- client/src/services/ContestServices.js
+++ client/src/services/ContestServices.js
@@ -3,5 +3,8 @@ import Api from '@/services/Api'
 export default {
   index () {
     return Api().get('contests')
+  },
+  post (contest) {
+    return Api().post('manage-contests', contest)
   }
 }
