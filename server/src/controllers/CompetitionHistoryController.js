f88badcbb6035ad0835fc8dd84f42354d3e8cc1e
Shaun-Shane <1953181@tongji.edu.cn>
2022-05-06 11:40:37 +0800

    create competitionhistory

---
+++ server/src/controllers/CompetitionHistoryController.js
@@ -0,0 +1,8 @@
+//const { Round, Content } = require('../models')
+
+module.exports = {
+  async show (req, res) {
+  },
+  async post (req, res) {
+  }
+}
---
+++ server/src/models/CompetitionHistory.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, DataTypes) => {
+  const CompetitionHistory = sequelize.define('CompetitionHistory', {
+    round_no: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    participant_id: {
+      type: DataTypes.STRING(255),
+      primaryKey: true
+    },
+    prev_rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    post_rating: {
+      type: DataTypes.INTEGER
+    },
+    rank: {
+      type: DataTypes.INTEGER
+    },
+    miss_count: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    type_progress: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    wpm: {
+      type: DataTypes.FLOAT(4, 1),
+      allowNull: false
+    },
+    score: {
+      type: DataTypes.INTEGER
+    }
+  }, {
+    timestamps: false
+  })
+  return CompetitionHistory
+}
