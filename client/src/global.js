fb5b421c59df6e9b1be3669c602ad1ff8575895d
Shaun-Shane <1953181@tongji.edu.cn>
2022-05-07 10:32:09 +0800

    Update global.js

--- client/src/global.js
+++ client/src/global.js
@@ -7,5 +7,22 @@ export default {
     let s2 = `${minutes < 10 ? `0${minutes}` : `${minutes}`}`
     let s3 = `${seconds < 10 ? `0${seconds}` : `${seconds}`}`
     return `${s1} : ${s2} : ${s3}`
+  },
+  setTimeTag (contests, serverTime) {
+    contests.forEach((item) => {
+      let timeDif = (new Date(item.start_time)).getTime() - serverTime
+      if (timeDif > 0) {
+        let days = parseInt(timeDif / (86400 * 1000))
+        if (days) item.timeTag = `<br/> Before Start <br/> ${days} ${days === 1 ? 'day' : 'days'}`
+        else {
+          item.timeTag = `<br/> Before Start <br/> ${this.timeDifToString(timeDif)}<br/>`
+        }
+      } else {
+        timeDif = (new Date(item.start_time)).getTime() - serverTime + item.duration * 60000
+        if (timeDif > 0) {
+          item.timeTag = `<br/> Before End <br/> ${this.timeDifToString(timeDif)}<br/>`
+        } else item.timeTag = `<br/> Final Standing <br/>`
+      }
+    })
   }
 }
