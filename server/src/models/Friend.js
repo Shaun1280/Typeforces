0c0978bdd6d532ca998696ed1df95541f51f9ce7
Shaun-Shane <1953181@tongji.edu.cn>
2022-06-09 21:59:11 +0800

    add message and friends

---
+++ server/src/models/Friend.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+  const Friend = sequelize.define('Friend', {
+    id1: {
+      type: DataTypes.STRING(255),
+      primaryKey: true
+    },
+    id2: {
+      type: DataTypes.STRING(255),
+      primaryKey: true,
+      validate: {
+        greaterThanId1 (value) {
+          if (value <= this.id1) {
+            throw new Error('id1 should be less than id2')
+          }
+        }
+      }
+    }
+  }, {
+    timestamps: true
+  })
+  return Friend
+}
---
+++ server/src/models/Message.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const Message = sequelize.define('Message', {
+    message_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    receiver_id: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    sender_id: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.STRING(40),
+      allowNull: false,
+      validate: {
+        greaterThanId1 (value) {
+          if (value.length > 40) {
+            throw new Error('message length can\'t be greater than 40')
+          }
+        }
+      }
+    },
+    send_time: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  }, {
+    timestamps: false
+  })
+  return Message
+}
